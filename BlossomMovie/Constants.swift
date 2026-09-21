a3bf811c2e96664a557cb6ed5f98849bb7979581
Sendiko <rizkysendiko7@gmail.com>
2026-09-21 15:12:13 +0700

    Implement text styling using Extension function

--- BlossomMovie/Constants.swift
+++ BlossomMovie/Constants.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Constants {
     
@@ -25,4 +26,17 @@ struct Constants {
     // URLs
     static let testTitleURL = "https://image.tmdb.org/t/p/w500/nnl6OWkyPpuMm595hmAxNW3rZFn.jpg"
 
+}
+
+extension Text {
+    func ghostButton() -> some View {
+        self
+            .frame(width: 100, height: 50)
+            .foregroundStyle(.buttonText)
+            .bold()
+            .background {
+                RoundedRectangle(cornerRadius: 20, style: .continuous)
+                    .stroke(.buttonBorder, lineWidth: 5)
+            }
+    }
 }
--- BlossomMovie/HomeView.swift
+++ BlossomMovie/HomeView.swift
@@ -25,17 +25,13 @@ struct HomeView: View {
                     
                 } label: {
                     Text(Constants.playString)
-                        .frame(width: 100, height: 50)
-                        .bold()
-                        .background {
-                            RoundedRectangle(cornerRadius: 20, style: .continuous)
-                                .stroke(lineWidth: 5)
-                        }
+                        .ghostButton()
                 }
                 Button {
                     
                 } label: {
                     Text(Constants.downloadString)
+                        .ghostButton()
                 }
             }
         }
