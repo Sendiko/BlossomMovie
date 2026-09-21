b74d684dfb5bd6147b944149b2f9e27a3b7b9e03
Sendiko <rizkysendiko7@gmail.com>
2026-09-21 19:02:50 +0700

    Give gradient to HomeView, calls HomeView from ContentView inside Home Tab

--- BlossomMovie/ContentView.swift
+++ BlossomMovie/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         TabView {
             Tab(Constants.homeString, systemImage: Constants.homeIcon) {
-                Text(Constants.homeString)
+                HomeView()
             }
             Tab(Constants.upcomingString, systemImage: Constants.upcomingIcon) {
                 Text(Constants.upcomingString)
