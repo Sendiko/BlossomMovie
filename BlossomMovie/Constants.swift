28a9a91911a0a4dcb837278f0600896b15044a2c
Sendiko <rizkysendiko7@gmail.com>
2026-09-21 14:04:24 +0700

    Create constants to minimalize raw string in code

---
+++ BlossomMovie/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  BlossomMovie
+//
+//  Created by mac on 21/09/26.
+//
+
+import Foundation
+
+struct Constants {
+    
+    // Strings
+    static let homeString = "Home"
+    static let upcomingString = "Upcoming"
+    static let searchString = "Search"
+    static let downloadString = "Downloads"
+    
+    // Icons
+    static let homeIcon = "house"
+    static let upcomingIcon = "play.circle"
+    static let searchIcon = "magnifyingglass"
+    static let downloadIcon = "arrow.down.to.line"
+    
+}
